b4f2f05a94a5f39c4186fc87354997b0a608b7fc
Rasel Ahmed <raselahmediu22@gmail.com>
2026-06-28 22:22:58 +0600

    add verify

--- src/lib/axiosSecure.js
+++ src/lib/axiosSecure.js
@@ -1,22 +1,21 @@
-"use client";
 import axios from "axios";
-import { authClient } from "@/lib/auth-client";
 
-const axiosSecure = axios.create({ 
-  baseURL: process.env.NEXT_PUBLIC_API_URL, 
-  withCredentials: true 
+const axiosSecure = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,  
 });
 
-axiosSecure.interceptors.request.use(async (config) => {
-  const session = await authClient.getSession();
-  const token = session?.data?.session?.token;
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+
 
 axiosSecure.interceptors.response.use(
   (response) => response,
-  (error) => Promise.reject(error)
+  (error) => {
+    if (error.response?.status === 401) {
+      console.log("Unauthorized - redirecting to login");
+      // window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default axiosSecure;
