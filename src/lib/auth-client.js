c9b26a736aa859fb8f8783f48ec78da262026c1e
Rasel Ahmed <raselahmediu22@gmail.com>
2026-06-28 15:07:36 +0600

    add auth js file

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,10 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  fetchOptions: {
-    credentials: "include",
-  },
+  baseURL: "https://resell-hub-rho.vercel.app",
 });
 
 export const { signIn, signUp, signOut, useSession, getSession } = authClient;
