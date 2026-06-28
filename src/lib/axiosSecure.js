768bb09651c52a5994c66ce05afd6ba5263670e7
Rasel Ahmed <raselahmediu22@gmail.com>
2026-06-28 16:52:41 +0600

    testing error

--- src/lib/axiosSecure.js
+++ src/lib/axiosSecure.js
@@ -23,9 +23,9 @@ axiosSecure.interceptors.request.use(async (config) => {
 axiosSecure.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
-      window.location.href = "/login";
-    }
+    // if (error.response?.status === 401) {
+    //   window.location.href = "/login";
+    // }
     return Promise.reject(error);
   }
 );
