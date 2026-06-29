ae09c5d6995f41018705936ff9e622864f087f79
Rasel Ahmed <raselahmediu22@gmail.com>
2026-06-29 16:02:08 +0600

    add simple axios file

--- src/lib/axiosSecure.js
+++ src/lib/axiosSecure.js
@@ -1,28 +1,8 @@
 import axios from "axios";
-import { authClient } from "./auth-client";
 
 const axiosSecure = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
-
-axiosSecure.interceptors.request.use(async (config) => {
-  const session = await authClient.getSession();
-  const token = session?.data?.session?.token;
-  if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`;
-  }
-  return config;
-});
-
-axiosSecure.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      console.log("Unauthorized - redirecting to login");
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default axiosSecure;
