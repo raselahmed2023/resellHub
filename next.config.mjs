fa0a48c882f096e87e228d2c77e12ab3c7e88a5a
Rasel Ahmed <raselahmediu22@gmail.com>
2026-06-29 21:44:37 +0600

    add google auth

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: "https://resell-hub-server-two.vercel.app/api/auth/:path*",
+      },
+      {
+        source: "/server/:path*",
+        destination: "https://resell-hub-server-two.vercel.app/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
